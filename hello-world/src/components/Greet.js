7d8d677b59fbd70204ed770464323aeec5a4fc84
Manav Deep Singh Lamba <manavdeep.singhlamba@gmail.com>
2021-09-03 02:58:39 +0530

    commit

---
+++ hello-world/src/components/Greet.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// function Greet(){
+//     return <h1>Hello, Manav !!</h1>
+// }
+
+// this was default export
+//  export default Greet
+
+export const Greet = ()=>{
+   return <h1>Hello, Manav !!</h1>
+}
