f307f8207109a7c8a046931a3582b837486cc95a
Manav Deep Singh Lamba <manavdeep.singhlamba@gmail.com>
2021-09-03 22:40:07 +0530

    commit

--- hello-world/src/components/ReactCreateMethod.js
+++ hello-world/src/components/ReactCreateMethod.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ReactCreateMethod = () => {
     return (
-        React.createElement('div', null, 
+        React.createElement('div', {className:'hello',style:{color:"red"}}, 
         React.createElement('h1', null, 'Hello from a functional component not using jsx and onlu using react create element methhod'))
     )
 }
