9298355eba36c2932531db7cb29bdd59506ca92e
Manav Deep Singh Lamba <manavdeep.singhlamba@gmail.com>
2021-09-03 22:17:27 +0530

    commit

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Greet } from './components/Greet'
 import Welcome from './components/Welcome'
+import Hello from './components/Hello'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       
         <Greet/>
         <Welcome />
+        <Hello />
       
     </div>
   );
---
+++ hello-world/src/components/Hello.js
@@ -0,0 +1,11 @@
+import react from 'react'
+
+function Hello()
+ {
+     return  (
+        <div>
+            <h1>Hello, World a simple react component</h1>
+        </div>
+     )
+ }
+ export default Hello
