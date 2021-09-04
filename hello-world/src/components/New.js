a91a378dbcfcf05779c6c7785b2d382461276cd5
Manav Deep Singh Lamba <manavdeep.singhlamba@gmail.com>
2021-09-04 14:01:30 +0530

    Create New.js

---
+++ hello-world/src/components/New.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class New extends Component {
+    render() {
+        return (
+            <div>
+                Hello world this my first ever react component from 5the new key board ,Yayyyyyy
+            </div>
+        )
+    }
+}
+
+export default New
